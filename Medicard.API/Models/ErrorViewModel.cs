6b96a97eabc463d20164816eb4d8f7a3a638a0f0
Andrij Matviiv <73823120+matviiv8@users.noreply.github.com>
2022-11-04 17:05:44 +0200

    Delete Medicard.API directory

--- Medicard.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Medicard.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
