6b93ce89d8107f909b964304106c7dc2771b5ff2
Andrij Matviiv <andrey777matvyiv@gmail.com>
2022-11-10 22:47:36 +0200

    Add controller and update view and viewmodel

--- Medicard.WebUI/Models/PatientProfileViewModel.cs
+++ Medicard.WebUI/Models/PatientProfileViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Medicard.WebUI.Models
+﻿using Medicard.Domain.Entities.Enums;
+
+namespace Medicard.WebUI.Models
 {
     public class PatientProfileViewModel
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string? ContactNumber { get; set; }
+
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? BirthDate { get; set; }
+
+        public MaritalStatus MaritalStatus { get; set; }
+
     }
 }
