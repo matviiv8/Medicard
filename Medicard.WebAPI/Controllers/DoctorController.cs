7adebef97cd0821485bbea196684c813c851833b
Andrij Matviiv <andrey777matvyiv@gmail.com>
2022-12-05 00:31:03 +0200

    integrated the method Get into the service

--- Medicard.WebAPI/Controllers/DoctorController.cs
+++ Medicard.WebAPI/Controllers/DoctorController.cs
@@ -1,6 +1,8 @@
 ﻿using Medicard.Domain.Astract;
 using Medicard.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace Medicard.WebAPI.Controllers
 {
@@ -15,6 +17,21 @@ namespace Medicard.WebAPI.Controllers
             _unitOfWork = unitOfWork;
         }
 
+        /// <summary>  
+        /// get all doctors
+        /// </summary>  
+        /// <param></param>  
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var doctors = _unitOfWork.GenericRepository<Doctor>().GetAll();
+            return Ok(doctors);
+        }
+
+        /// <summary>  
+        /// get doctor by id  
+        /// </summary>  
+        /// <param name="id">id</param>  
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
@@ -28,6 +45,10 @@ namespace Medicard.WebAPI.Controllers
             return NotFound();
         }
 
+        /// <summary>  
+        /// delete doctor  
+        /// </summary>  
+        /// <param name="id">id</param>  
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
@@ -41,6 +62,37 @@ namespace Medicard.WebAPI.Controllers
             }
 
             return NotFound();
+        }
+        /// <summary>  
+        /// update doctor 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <param name="doctor">object doctor</param> 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Doctor doctor)
+        {
+            if (doctor == null)
+            {
+                return BadRequest();
+            }
+            if (!_unitOfWork.GenericRepository<Doctor>().GetAll().Any(x => x.Id == id))
+            {
+                return NotFound();
+            }
+
+            var newDoctor = new Doctor()
+            {
+                FirstName = doctor.FirstName,
+                LastName = doctor.LastName,
+                ContactNumber = doctor.ContactNumber,
+                Age = doctor.Age,
+                Specialization = doctor.Specialization,
+                Gender = doctor.Gender,
+                DoctorPicture = doctor.DoctorPicture,
+            };
+            _unitOfWork.GenericRepository<Doctor>().Update(newDoctor);
+            await _unitOfWork.SaveAsync();
+            return Ok(newDoctor);
         }
     }
 }
--- Medicard.WebAPI/Program.cs
+++ Medicard.WebAPI/Program.cs
@@ -1,6 +1,8 @@
 using Medicard.Domain.Astract;
 using Medicard.Domain.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Medicard API",
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<MedicardDbContext>(options =>
 {
@@ -25,11 +38,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medicard API v1"));
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
