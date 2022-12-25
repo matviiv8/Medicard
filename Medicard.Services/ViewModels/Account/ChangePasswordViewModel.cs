44d72baca48136f639d4f2448d1560adc4cf73d9
Andrij Matviiv <andrey777matvyiv@gmail.com>
2022-12-25 02:09:25 +0200

    added ChangePassword and DeletePersonalData

---
+++ Medicard.Services/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Medicard.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicard.Services.ViewModels.Account
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        public User User { get; set; }
+    }
+}
